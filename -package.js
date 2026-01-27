7f75294ac7aa4c09e1b3476f8b3445e1bdbb1816
Mubarak Ashry <mobarak.ashry@gmail.com>
2026-01-27 08:36:49 +0200

    Add Pi SDK integration and updated lessons

---
+++ -package.js
@@ -0,0 +1,13 @@
+{
+  "name": "skill-spark-payment",
+  "version": "1.0.0",
+  "main": "index.js",
+  "type": "module",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2",
+    "axios": "^1.6.7"
+  }
+}
